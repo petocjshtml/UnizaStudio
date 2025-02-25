93e037e99d155a27ed53423473c7083ea71aa1ba
petocjshtml <speter987654321@gmail.com>
2025-02-25 15:55:47 +0100

    spring boot uvodny commit

---
+++ src/main/java/com/example/UnizaStudio/UnizaStudioApplication.java
@@ -0,0 +1,13 @@
+package com.example.UnizaStudio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UnizaStudioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UnizaStudioApplication.class, args);
+	}
+
+}
